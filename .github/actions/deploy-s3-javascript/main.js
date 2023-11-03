cb3566606c63a8b3289e622ce6aff6b1f6608cdf
mitchell_kimbell <mfkimbell@gmail.com>
2023-11-03 13:31:29 -0500

    Update main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
-const core = require("@actions/github");
-const core = require("@actions/exec");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 function run() {
   core.notice("Hello from my custom Javascript Action!");
