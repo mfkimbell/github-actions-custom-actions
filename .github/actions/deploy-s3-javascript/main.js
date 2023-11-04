b8ec24e81d56ab9db3e1178ab6377ede0a1945ef
mitchell_kimbell <mfkimbell@gmail.com>
2023-11-04 16:20:46 -0500

    added custom deploy action to S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,11 +1,26 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+//const github = require("@actions/github");
+//could use for github context object
 const exec = require("@actions/exec");
+//these are github actions that we can use to connect to AWS
 
 function run() {
+  // Get input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
 
-	//get input values
-	
+  // Upload files
+  // could use -> const localFolder = distFolder
+  // we are accessing the s3 bucket and then syncing a local folder
+  // with the s3 bucket
+  s3Uri = `s3://${bucket}`;
+	// This function recieves its permissions from enviornment variables
+	//
+	// the github runner, AWS-CLI looks for keys with
+ 	// specific names when making calls, ex: AWS_SECRET_KEY 
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Hello from my custom Javascript Action!");
 }
 
